dde19a010cb52428d7001e66cb727ff3ecdb2d8e
surya kant <suryakant0013@yahoo.in>
2023-08-23 00:24:15 +0530

    added a user query in router

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,13 @@
-import {router} from './trpc';
+import { router, publicProcedure } from './trpc';
+import { db } from './db';
 
 const appRouter = router(
-    {}
+    {
+        userlist: publicProcedure.query(async () => {
+            const users = await db.user.findMany();
+            return users;
+        })
+    }
 );
 
 export type AppRouter = typeof appRouter;
