2613ca2410c4cbeada98d3e0438e45bc3e6c67cb
surya kant <suryakant0013@yahoo.in>
2023-08-23 00:40:39 +0530

    added userById query and userCreate mutation

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,28 @@
 import { router, publicProcedure } from './trpc';
 import { db } from './db';
+import { z } from 'zod';
 
 const appRouter = router(
     {
         userlist: publicProcedure.query(async () => {
             const users = await db.user.findMany();
             return users;
-        })
+        }),
+
+        userById: publicProcedure.input(z.string()).query(async (opts) => {
+            const { input } = opts;
+            const user = await db.user.findById(input);
+            return user;
+        }),
+        
+        userCreate: publicProcedure
+            .input(z.object({ name: z.string() }))
+            .mutation(async (opts) => {
+                const { input } = opts;
+                const user = await db.user.create(input);
+                return user;
+            })
+
     }
 );
 
